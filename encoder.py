0fd3b83a6bd114a8712c6924ffd54b732ea70c25
aziz16abdellaoui <114643423+aziz16abdellaoui@users.noreply.github.com>
2025-04-16 13:29:06 +0100

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -10,5 +10,6 @@ def hamming_encode(bits):
         p1 = d[0] ^ d[1] ^ d[3]
         p2 = d[0] ^ d[2] ^ d[3]
         p3 = d[1] ^ d[2] ^ d[3]
+        
         encoded.extend([p1, p2, d[0], p3, d[1], d[2], d[3]])
     return np.array(encoded)
